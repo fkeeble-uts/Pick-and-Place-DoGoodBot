f500b05d3af71b16638b50ff63aaeb0bfc09e043
leoo-c1 <chinchillaleo01@gmail.com>
2025-10-09 11:55:20 +1100

    Correctly positioned all links and adjusted colour

--- IngredientBot.py
+++ IngredientBot.py
@@ -15,14 +15,14 @@ class IngredientBot(DHRobot3D):
     def __init__(self):
         links = self._create_DH()
         link3D_names = dict(
-            link0='CRB15000_Joint1', color0=(0.2, 0.2, 0.2, 1),
-            link1='CRB15000_Joint2', color1=(0.1, 0.1, 0.1, 1),
-            link2='CRB15000_Joint3', color2=(0.9, 0.9, 0.9, 1),
-            link3='CRB15000_Joint4', color3=(0.2, 0.2, 0.2, 1),
-            link4='CRB15000_Joint5', color4=(0.2, 0.2, 0.2, 1),
-            link5='CRB15000_Joint6', color5=(0.2, 0.2, 0.2, 1),
-            link6='CRB15000_Joint7', color6=(0.2, 0.2, 0.2, 1),
-            link7='CRB15000_Joint7', color7=(0.2, 0.2, 0.2, 1)
+            link0='CRB15000_Joint1', color0=(0.4, 0.45, 0.5, 1),
+            link1='CRB15000_Joint2', color1=(0.4, 0.45, 0.5, 1),
+            link2='CRB15000_Joint3', color2=(0.4, 0.45, 0.5, 1),
+            link3='CRB15000_Joint4', color3=(0.4, 0.45, 0.5, 1),
+            link4='CRB15000_Joint5', color4=(0.8, 0.8, 0.8, 1),
+            link5='CRB15000_Joint6', color5=(0.8, 0.8, 0.8, 1),
+            link6='CRB15000_Joint7', color6=(0.8, 0.8, 0.8, 1),
+            link7='CRB15000_Joint7', color7=(0.8, 0.8, 0.8, 1)
         )
         qtest = [0, 0, -pi/2, 0, 0, pi, 0]
         qtest_transforms = [spb.transl(0, 0, 0) @ spb.rpy2tr(0, 0, -pi/2, order='xyz'),
@@ -30,9 +30,9 @@ class IngredientBot(DHRobot3D):
                             spb.transl(0.01405, -0.0001, 0.0151) @ spb.rpy2tr(0, 0, -pi/2, order='xyz'),
                             spb.transl(0.014, -0.0001, 0.0012) @ spb.rpy2tr(0, 0, -pi/2, order='xyz'),
                             spb.transl(0.0139, -0.0001, -0.00463) @ spb.rpy2tr(0, 0, -pi/2, order='xyz'),
-                            spb.transl(0, 0, 0) @ spb.rpy2tr(0, pi/2, -pi/2, order='xyz'),
-                            spb.transl(0, 0, 0) @ spb.rpy2tr(0, pi/2, -pi/2, order='xyz'),
-                            spb.transl(0, 0, 0) @ spb.rpy2tr(0, pi/2, -pi/2, order='xyz')]
+                            spb.transl(0.0139, -0.0001, -0.0007) @ spb.rpy2tr(0, 0, -pi/2, order='xyz'),
+                            spb.transl(0.0139, -0.0001, -0.0048) @ spb.rpy2tr(0, 0, -pi/2, order='xyz'),
+                            spb.transl(0, 0, 0) @ spb.rpy2tr(0, 0, 0, order='xyz')]
         # rotation: robot y = global z, robot z = global y, robot x = global x
         # translation: robot x = global y, robot y = global z, robot z = global x
         current_path = os.path.abspath(os.path.dirname(__file__))
@@ -85,9 +85,6 @@ if __name__ == "__main__":
 
     robot_arm = IngredientBot()
     robot_arm.add_to_env(env)
-
-    # Set a reasonable camera view
-    env.set_camera_pose([1.5, 1.5, 1.0], [0, 0, 0])
 
     # Keep the simulation running
     while True:
