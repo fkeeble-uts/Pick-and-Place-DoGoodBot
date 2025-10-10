105671d5ff8e10a9b236d36b28850f3c22b5a4f0
leoo-c1 <chinchillaleo01@gmail.com>
2025-10-10 21:16:47 +1100

    Correct incorrect theta values for DH parameters, minor adjustments to align links

--- IngredientBot.py
+++ IngredientBot.py
@@ -22,15 +22,15 @@ class IngredientBot(DHRobot3D):
             link6='CRB15000_Joint6', color6=(0.8, 0.8, 0.8, 1),
             link7='CRB15000_Joint6', color7=(0.8, 0.8, 0.8, 1)
         )
-        qtest = [0, 0, pi/2, 0, 0, pi]
+        qtest = [0, pi/2, 0, 0, pi, 0]
         qtest_transforms = [spb.transl(0, 0, 0),
                             spb.transl(0.0625, -0.1375, 0.2141),
                             spb.transl(0.211875, -0.1512, 0.317),
                             spb.transl(0.2263, -0.0115, 1.0385),
                             spb.transl(0.3895, 0.048, 1.161),
                             spb.transl(0.8, 0.0143, 1.161),
-                            spb.transl(0.996, 0.056, 1.242),
-                            spb.transl(0.996, 0.056, 1.242)] @ spb.transl(-0.14385, -0.102, 0)
+                            spb.transl(0.996, 0.0565, 1.2425),
+                            spb.transl(0.996, 0.0565, 1.2425)] @ spb.transl(-0.1433, -0.101, 0)
 
         current_path = os.path.abspath(os.path.dirname(__file__))
 
@@ -47,8 +47,9 @@ class IngredientBot(DHRobot3D):
 
     def _create_DH(self):
         links = []
-        a = [0.15, -0.707, -0.110, 0, 0.08, 0, 0]
-        d = [0.4, -0.0863, -0.0863, 0.637, 0, 0.101, 0]
+        # qtest = [0, 0, pi/2, 0, 0, pi]
+        d = [0.399, -0.0863, -0.0863, 0.636, 0.0085, 0.101, 0]
+        a = [0.15, -0.706, -0.110, 0, 0.0805, 0, 0]
         alpha = [-pi/2, pi, -pi/2, -pi/2, -pi/2, 0, 0]
         qlim = [[-2*pi, 2*pi] for _ in range(6)]
         for i in range(6):
